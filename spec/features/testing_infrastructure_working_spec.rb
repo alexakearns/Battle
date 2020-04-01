594aca8104dad06b8f396c6b9b1c43ac413f02d0
Alexa <alexakearns@outlook.com>
2020-04-01 16:46:26 +0100

    test infrastructire working

---
+++ spec/features/testing_infrastructure_working_spec.rb
@@ -0,0 +1,8 @@
+require "spec_helper"
+
+feature "testing infrastructure is working" do
+  scenario "Infrastructure works" do
+    visit('/')
+    expect(page).to have_content 'Testing infrastructure working!'
+  end
+end
